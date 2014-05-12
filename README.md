5a06f86dc03397fcb6d45720f03bbb2872811dfe
Simon Boyer <simon.boyer@openwide.fr>
2014-05-12 12:09:07 +0200

    Add default instrument values replacement

--- README.md
+++ README.md
@@ -8,3 +8,15 @@ How to install
 Simply download zip file or clone repo :
   git clone https://github.com/sboye/Hydrogen-Drumkit-Generator.git
 
+Usage
+----------------------
+./generate_drumkit.sh [--key=value] [SAMPLE_DIR] [NAME_OF_DRUMKIT]
+
+[--key=value] will replace any default value in xml instrument.txt template.
+
+See ressources/instrument.txt for full options list.
+
+Exemple
+------------------------
+./generate_drumkit.sh --pitch=8 --Decay=13 /path/to/my/ogg/samples/ name_of_my_drumkit
+
--- generate_drumkit.sh
+++ generate_drumkit.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
+declare -A replaces
+while [ $# -gt 0 ]
+do
+      case "$1" in
+              #-v)  vflag=on;;
+              #-f)  filename="$2"; shift;;
+              --*)  parameter=${1#--}
+                    key=${parameter%=*}
+                    value=${parameter#*=}
+                    replaces[${key}]=${value}
+                    ;;
+              -*)
+                echo >&2 \
+                   "usage: $0 [--key=value] [SAMPLE_DIR] [NAME_OF_DRUMKIT]"
+                exit 1;;
+              *)  break;;
+              #      terminate while loop
+      esac
+      shift
+done
+
 SAMPLES_DIR=$1
-#TARGET_DIR=$2
 NAME=$2
 HYDROGEN_DIR="$HOME/.hydrogen/data/drumkits"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#EZDIR=$2
-echo $DIR
+
 if [[ -d $1 ]]
 then
 	cd $SAMPLES_DIR
@@ -22,11 +41,15 @@ then
     echo "Copying $file to $HYDROGEN_DIR/$NAME/$file"
 		instrumentlist="$instrumentlist `cat $SCRIPT_DIR/ressources/instrument.txt | sed -e "s/__FILENAME__/$file/g"`"
 	done
+  for i in "${!replaces[@]}"
+  do
+    instrumentlist=`printf "%s" "$instrumentlist" | sed -e "s#\(<$i>\)\(.*\)\(</$i>\)#\1${replaces[$i]}\3#g"`
+  done
   cat $SCRIPT_DIR/ressources/instrumentlist_header.txt | sed -e "s/__NAME__/$NAME/" > $HYDROGEN_DIR/$NAME/drumkit.xml
   printf "%s" "$instrumentlist" >> $HYDROGEN_DIR/$NAME/drumkit.xml
   cat $SCRIPT_DIR/ressources/instrumentlist_footer.txt >> $HYDROGEN_DIR/$NAME/drumkit.xml
 )
 else
-	echo "Directory $i is not valid."
-  echo "     Calling : ./hydrogenkit.sh [SAMPLE_DIR] [NAME_OF_DRUMKIT]"
+	echo "Directory $1 is not valid."
+  echo "usage: $0 [--key=value] [SAMPLE_DIR] [NAME_OF_DRUMKIT]"
 fi
